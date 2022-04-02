1471068ac8250f3a00d941ff368ffc28fea15fc9
Boris <bfleyshe@ualberta.ca>
2022-04-02 17:54:44 -0600

    Fixed warnings

--- SmashCharacterSelect/CharacterPicker.gd
+++ SmashCharacterSelect/CharacterPicker.gd
@@ -11,7 +11,7 @@ func _ready():
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _process(_delta):
 	var overlappingCharacters = []
 	for i in $Token/Area2D.get_overlapping_areas():
 		if i.is_in_group("selectableCharacter"):
